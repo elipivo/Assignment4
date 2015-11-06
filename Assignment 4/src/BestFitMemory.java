2427296f522b6d29905749e18e913bfb52139265
NextBillyonair <NextBillyonair@Billy>
2015-11-06 12:19:56 -0500

    Bestfit

---
+++ Assignment 4/src/BestFitMemory.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+/**
+ * Interface Memory.java.
+ * CS 600.226 Data Structures Fall 2015
+ * Assignment 4
+ * Eli Pivo - epivo1
+ * Raphael Norman-Tenazas - rtenaza1
+ * William Watson - wwatso13
+ */
+public class BestFitMemory implements Memory{
+    
+    @Override
+    public int allocate(int size, int allocNum) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean deallocate(int allocNum) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void defrag() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public ArrayList<Block> bucketSort() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ArrayList<Block> quickSort() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+}
